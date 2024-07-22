92639f59c8c4e72adf5eea6aafdd1f879d38a83a
ulmeck <ulm@appin.org>
2024-07-22 10:20:24 -0500

    Add files via upload

--- .github/set-img-root.sh
+++ .github/set-img-root.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sed -i 's#DEPLOYED_IMG_ROOT\s*=\s*undefined#DEPLOYED_IMG_ROOT='"\"https://raw.githubusercontent.com/5etools-mirror-2/5etools-img/main/\""'#g' js/utils.js
+sed -i 's#DEPLOYED_IMG_ROOT\s*=\s*undefined#DEPLOYED_IMG_ROOT='"\"https://raw.githubusercontent.com/ulmeck/5etools-img/main/\""'#g' js/utils.js
