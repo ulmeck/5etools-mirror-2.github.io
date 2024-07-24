f0e737921bc39020b4b6fbd718d1c69de9a7db41
ulmeck <ulm@appin.org>
2024-07-23 19:05:16 -0500

    Update set-img-root.sh

--- .github/set-img-root.sh
+++ .github/set-img-root.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sed -i 's#DEPLOYED_IMG_ROOT\s*=\s*undefined#DEPLOYED_IMG_ROOT='"\"https://raw.githubusercontent.com/5etools-mirror-2/5etools-img/main/\""'#g' js/utils.js
+sed -i 's#DEPLOYED_IMG_ROOT\s*=\s*undefined#DEPLOYED_IMG_ROOT='"\"https://5etools.local/5etools-img/\""'#g' js/utils.js
